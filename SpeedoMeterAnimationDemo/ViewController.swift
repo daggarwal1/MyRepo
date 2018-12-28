835859ccfee26625899158d79e50045609d22dc7
Deepali Aggarwal <deepaliaggarwal@Deepali-IPhone.local>
2018-12-28 17:30:35 +0530

    This is a new commit for what I originally planned to be amended

---
+++ SpeedoMeterAnimationDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SpeedoMeterAnimationDemo
+//
+//  Created by Deepali Aggarwal on 20/07/18.
+//  Copyright © 2018 Deepali Aggarwal. All rights reserved.
+//
+
+import UIKit
+import ABGaugeViewKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
